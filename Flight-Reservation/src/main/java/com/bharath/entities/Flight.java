bd650ed0b63dc0b7b08b10faf872b6b6873d587c
Shahid <shahidyamdi@gmail.com>
2023-06-17 12:27:02 -0700

    notes

--- Flight-Reservation/src/main/java/com/bharath/entities/Flight.java
+++ Flight-Reservation/src/main/java/com/bharath/entities/Flight.java
@@ -4,22 +4,24 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Flight extends AbstractEntity{
-	
+public class Flight extends AbstractEntity {
+
 	private String flightNumber;
 	private String operatingAirlines;
 	private String departureCity;
 	private String arrivalCity;
+	@DateTimeFormat(pattern = "DD-MM-YYYY")
+	@Temporal(TemporalType.DATE)
 	private Date dateOfDeparture;
 	private Timestamp estimatedDepartureTime;
-
-
-	
-	
 
 }
